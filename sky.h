3b69f4b49b32c591aab17675d9ef122e1d7f7478
Md-Takibuddin <gamertakib@gmail.com>
2022-04-15 11:07:16 +0600

    Done

---
+++ sky.h
@@ -0,0 +1,217 @@
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include<cstdio>
+#include <windows.h>
+#include<math.h>
+# define PI 3.14159265358979323846
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void fullsky()
+{
+     glLoadIdentity();
+    glBegin(GL_POLYGON);
+
+    glColor3ub(30,144,255);
+        glVertex2f(-1.0f, 1.0f);
+        glVertex2f(1.0f, 1.0f);
+    glColor3ub(135,206,235);
+        glVertex2f(1.0f, -0.5f);
+	glColor3ub(135,206,235);
+        glVertex2f(-1.0f, -0.5f);
+
+	glEnd();
+}
+
+void bird()
+{
+  glBegin(GL_LINES);
+  glColor3ub(255,255,255);
+  glVertex2f(0.2f,0.8f);
+  glVertex2f(0.18f,0.77f);
+
+  glColor3ub(255,255,255);
+  glVertex2f(0.18f,0.77f);
+  glVertex2f(0.15f,0.79f);
+
+  glColor3ub(255,255,255);
+  glVertex2f(0.13f,0.8f);
+  glVertex2f(0.11f,0.77f);
+
+  glColor3ub(255,255,255);
+  glVertex2f(0.11f,0.77f);
+  glVertex2f(0.08f,0.79f);
+
+  glEnd();
+
+}
+void sky1()
+{
+    // glLoadIdentity();
+    int i;
+
+	GLfloat x=.5f; GLfloat y=.8f; GLfloat radius =.05f;
+	int triangleAmount = 20;
+	GLfloat twicePi = 2.0f * PI;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(x, y); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+GLfloat a=.55f; GLfloat b=.78f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(a, b); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            a + (radius * cos(i *  twicePi / triangleAmount)),
+			    b + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat c=.45f; GLfloat d=.78f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(c, d); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            c + (radius * cos(i *  twicePi / triangleAmount)),
+			    d + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat e=.52f; GLfloat f=.75f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(e, f); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            e + (radius * cos(i *  twicePi / triangleAmount)),
+			    f+ (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat g=.6f; GLfloat h=.77f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(g, h); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            g + (radius * cos(i *  twicePi / triangleAmount)),
+			    h+ (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+
+}
+
+void sky2()
+{
+    // glLoadIdentity();
+    int i;
+
+	GLfloat x=-.5f; GLfloat y=.8f; GLfloat radius =.05f;
+	int triangleAmount = 20;
+	GLfloat twicePi = 2.0f * PI;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(x, y); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+GLfloat a=-.55f; GLfloat b=.78f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(a, b); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            a + (radius * cos(i *  twicePi / triangleAmount)),
+			    b + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat c=-.45f; GLfloat d=.78f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(c, d); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            c + (radius * cos(i *  twicePi / triangleAmount)),
+			    d + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat e=-.52f; GLfloat f=.75f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(e, f); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            e + (radius * cos(i *  twicePi / triangleAmount)),
+			    f+ (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+	GLfloat g=-.6f; GLfloat h=.77f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	    glColor3ub(30, 217, 255);
+		glVertex2f(g, h); // center of circle
+		for(i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            g + (radius * cos(i *  twicePi / triangleAmount)),
+			    h+ (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+
+}
+
+
+
+void nightsky()
+{
+     glLoadIdentity();
+    glBegin(GL_POLYGON);
+
+    glColor3ub(25,25,112);
+        glVertex2f(-1.0f, 1.0f);
+        glVertex2f(1.0f, 1.0f);
+
+    glColor3ub(100,149,237);
+        glVertex2f(1.0f, -0.5f);
+	glColor3ub(100,149,237);
+        glVertex2f(-1.0f, -0.5f);
+
+	glEnd();
+}
+
